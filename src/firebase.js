eb70c98d0b8e3d1004c7bfa0b48a0be42ef0fe60
Saloni Gupta <130276709+Saloni-gupta23@users.noreply.github.com>
2024-08-05 00:47:58 +0530

    Updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
-// src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBmAWQZlTSfn6-nRgH1_2ILJM6doFuVKPg",
+    authDomain: "vin-scanner-3c3ec.firebaseapp.com",
+    databaseURL: "https://vin-scanner-3c3ec.firebaseio.com",
+    projectId: "vin-scanner-3c3ec",
+    storageBucket: "vin-scanner-3c3ec.appspot.com",
+    messagingSenderId: "23382058222",
+    appId: "1:233820582222:web:17bc69c9c5df58ddfcae1f"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getDatabase(app);
 
 export { db };
